8231d6964a01b74282dabffdb1ea384782b88799
Shameek <shameek@umich.edu>
2018-09-14 15:57:09 -0400

    Submission

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {extractDigits} from './logic/Helpers';
+import TextField from '@material-ui/core/TextField';
+
+class App extends Component {
+  state = {
+    error: false,
+    output: ''
+  }
+
+  updateOutput = output => event => {
+    var _newVal = extractDigits(event.target.value);
+    this.setState({
+      error: _newVal == "" ? true : false,
+      output: _newVal,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <TextField
+          id="name"
+          label="Type any string to extact its digits"
+          margin="normal"
+          error={this.state.error}
+          fullWidth
+          onChange={this.updateOutput('output')}
+          />
+        <div className="App">
+          <h1 className="App-title">{this.state.output}</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/logic/Helpers.js
@@ -0,0 +1,33 @@
+export const extractDigits = (input) => {
+    var _subNum = "";
+    var _totalSum = 0;
+    var _containsNum = false;
+    var index = 0;
+    while (index < input.length)
+    {
+        while (_isNumber(input[index]))
+        {
+            _containsNum = true;
+            _subNum += input[index];
+            index++;
+        }
+        if(_subNum)
+        {
+            _totalSum += parseFloat(_subNum);
+            _subNum = "";
+        }
+        if(_isNegative(input[index]))
+            _subNum += input[index];
+
+        ++index;
+    }
+    return _totalSum || _containsNum ? _totalSum : "";
+}
+
+const _isNumber = (c) => {
+  return (c >= '0' && c <= '9');
+}
+
+const _isNegative = (c) => {
+  return c === '-';
+}
